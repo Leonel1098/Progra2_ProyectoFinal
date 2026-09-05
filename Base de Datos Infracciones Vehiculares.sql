66e93867d1ee617271065e05665565353b796efa
Leonel <gonzalezleonel1098@gmail.com>
2026-09-05 00:25:08 -0600

    CHANGE_LEONEL_05092026_TABLA_USUARIO

--- Base de Datos Infracciones Vehiculares.sql
+++ Base de Datos Infracciones Vehiculares.sql
@@ -48,3 +48,69 @@ estado boolean not null
 
 INSERT INTO regla_descuento (porcentaje, dias_limite, estado)
 VALUES (50.00, 4, true);
+
+CREATE TABLE persona(
+id_persona int auto_increment primary key,
+dpi_persona varchar(20) not null unique,
+nombre_persona varchar(250) not null,
+apellido_persona varchar (250) not null,
+telefono varchar(50) not null,
+direccion varchar(250) not null
+);
+
+CREATE TABLE usuario(
+id_usuario int auto_increment primary key,
+usuario varchar(20) not null unique,
+contraseña varchar(250) not null,
+estado boolean not null,
+id_persona int not null unique, 
+id_rol int not null,
+foreign key (id_persona) references persona(id_persona),
+foreign key(id_rol) references rol(id_rol)
+);
+
+
+CREATE TABLE vehiculo(
+id_vehiculo int auto_increment primary key,
+placa_vehiculo varchar(50) not null unique,
+marca varchar(50) not null,
+modelo varchar(50) not null,
+color varchar(50) not null
+);
+
+CREATE TABLE infraccion(
+id_infraccion int auto_increment primary key,
+numero_infraccion varchar(50) not null unique,
+fecha_infraccion date not null,
+direccion_infraccion varchar(250) not null,
+monto_original decimal(10,2) not null,
+id_persona int not null,
+id_vehiculo int not null,
+id_usuario int not null,
+id_tipo_infraccion int not null,
+id_estado int not null,
+foreign key(id_persona) references persona(id_persona),
+foreign key(id_vehiculo) references vehiculo(id_vehiculo),
+foreign key(id_usuario) references usuario(id_usuario),
+foreign key(id_tipo_infraccion) references tipo_infraccion(id_tipo_infraccion),
+foreign key(id_estado) references estado_infraccion(id_estado)
+);
+
+CREATE TABLE pago(
+id_pago int auto_increment primary key,
+id_infraccion int not null,
+monto_inicial decimal(10,2) not null,
+porcentaje_descuento decimal(5,2) not null,
+monto_descuento decimal(10,2) not null,
+monto_pagado decimal(10,2) not null,
+fecha_pago date not null,
+foreign key(id_infraccion) references infraccion(id_infraccion)
+);
+
+
+
+
+
+
+
+
